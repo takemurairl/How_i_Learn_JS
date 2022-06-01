8d5d41445d591719d8ac3d31a1e047befb5a9808
UltraMachine <takemurastream@gmail.com>
2022-06-01 20:59:55 +0300

    Short objects

---
+++ .vscode/Udemy_js_l3.js
@@ -0,0 +1,17 @@
+const name = 'RedStorm'
+const postQty = 26
+
+// const userProfile = {
+//     name: name,
+//     postQty: postQty,
+//     hasSignedAgreement: false
+
+// }
+
+const userProfile = {
+    name,
+    postQty,
+    hasSignedAgreement: false
+}
+
+console.log(userProfile)
